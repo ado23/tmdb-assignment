8feb0ab9995075a2f14e4e5fee494c761e4c0da0
Admir <28600628+ado23@users.noreply.github.com>
2024-05-03 00:36:58 +0200

    Setup testing in project.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig, UserConfig } from "vite";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/test/vitest.setup.ts"]
+  },
 
   server: {
     port: 3000,
@@ -13,4 +18,4 @@ export default defineConfig({
       usePolling: true
     }
   }
-});
+} as UserConfig);
