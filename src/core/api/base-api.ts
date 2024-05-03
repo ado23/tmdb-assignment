7ce4accda6ebd3e623fe6885ca8df2839707c120
Admir <28600628+ado23@users.noreply.github.com>
2024-05-03 19:27:06 +0200

    Added typescript support for api, and added axios config with interceptors.

---
+++ src/core/api/base-api.ts
@@ -0,0 +1,42 @@
+import { AxiosError } from "axios";
+
+import { Api, AuthenticationValidateKeyError } from "src/core/api/generatedApi";
+
+const { instance, v3: BaseApi } = new Api();
+
+const errorHandler = (error: AxiosError<AuthenticationValidateKeyError>) => {
+  console.log(error);
+};
+
+export const initializeHttpInterceptors = () => {
+  instance.interceptors.request.use(
+    function (config) {
+      /* Do something before request is sent */
+      config.headers.Authorization = `Bearer ${import.meta.env.VITE_TMDB_API_TOKEN}`;
+
+      return config;
+    },
+    function (error: AxiosError<AuthenticationValidateKeyError>) {
+      /* Do something with request error */
+      errorHandler(error);
+      return Promise.reject(error.message);
+    }
+  );
+
+  instance.interceptors.response.use(
+    function (response) {
+      /* Any status code that lie within the range of 2xx cause this function to trigger */
+      /* Do something with response data */
+      return response;
+    },
+    function (error: AxiosError<AuthenticationValidateKeyError>) {
+      /* Any status codes that falls outside the range of 2xx cause this function to trigger */
+      /* Do something with response error */
+
+      errorHandler(error);
+      return Promise.reject(error.message);
+    }
+  );
+};
+
+export default BaseApi;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "src/App.tsx";
+import { initializeHttpInterceptors } from "src/core/api/base-api";
+
 import "src/index.css";
+
+initializeHttpInterceptors();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App />
   </BrowserRouter>
 );
